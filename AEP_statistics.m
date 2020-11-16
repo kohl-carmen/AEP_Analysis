9594886315dc7ffca2e9e98caa8ac2946b266b18
Carmen <kohl.carmen.1@gmail.com>
2020-11-16 12:45:23 -0500

    stats for each peak (amp and lat)

--- AEP_statistics.m
+++ AEP_statistics.m
@@ -108,13 +108,13 @@ for t = 1:length(time)
     for tone_categ=1:length(Categ)
         for hemi=1:length(Hemi)
             if hemi==1%right hemi
-                if tone_categ==1;%contra
+                if tone_categ==1%contra
                     tone_side=2;%left tone
                 else%ipsi
                     tone_side=1;%right
                 end
             else%left hemi
-                 if tone_categ==1;%contra
+                 if tone_categ==1%contra
                     tone_side=1;
                 else%ipsi
                     tone_side=2;
@@ -127,7 +127,7 @@ for t = 1:length(time)
                 elseif test==2
                     if t>ind1 & t<ind2
                         dataoi=Data.(strcat('S',num2str(partic))).(Tone_side{tone_side}).(Hemi{hemi})(round((t-slope_ind_with/2:t+slope_ind_with/2)));
-                        P=polyfit(time(t-slope_ind_with/2:t+slope_ind_with/2)',dataoi,1);
+                        P=polyfit(time(round(t-slope_ind_with/2:t+slope_ind_with/2)'),dataoi,1);
                         data(partic,count)=P(1);
                     else
                         data(partic,count)=0;
@@ -495,7 +495,7 @@ legend(line,{'Hemi r', 'Hemi l'})
 Partic=1:10;
 Tone_side={'RE','LE'};
 Hemi={'rig','lef'};
-time=linspace(-201.000005, 793.699980,207)
+time=linspace(-201.000005, 793.699980,207);
 
 %for ANOVA   
 within1=categorical([1 1 0 0])';
@@ -575,7 +575,7 @@ for tone_side=1:length(Tone_side)
 %             plot(time,data)
             data_oi(partic,:)=data;
         end
-        line(count)=plot(time,Data.AVE.(Tone_side{tone_side}).(Hemi{hemi}),'Color',colour,'Linestyle',linestyle,'Linewidth',2)
+        line(count)=plot(time,Data.AVE.(Tone_side{tone_side}).(Hemi{hemi}),'Color',colour,'Linestyle',linestyle,'Linewidth',2);
             %make standard error
             SE_upper=[];
             SE_lower=[];
@@ -623,13 +623,13 @@ set(h,'EdgeColor', 'none');
 h.FaceColor=[0.8 .8 .8];
 sig_t=sig_temp(i);
 
-line=[]
-count=0'
+line=[];
+count=0;
 for tone_side=1:length(Tone_side)
     if tone_side==1
         colour=[.625 0 0];
     else
-        colour=[0 .312 .625]
+        colour=[0 .312 .625];
     end
     for hemi=1:length(Hemi)
         if hemi==1
@@ -637,7 +637,7 @@ for tone_side=1:length(Tone_side)
         else
             linestyle=':';
         end
-        count=count+1
+        count=count+1;
         set(gca, 'YDir','reverse')
         ylabel('Amplitude (nAm)')
         xlabel('Time (ms)')
@@ -649,7 +649,7 @@ for tone_side=1:length(Tone_side)
 %             plot(time,data)
             data_oi(partic,:)=data;
         end
-        line(count)=plot(time,Data.AVE.(Tone_side{tone_side}).(Hemi{hemi}),'Color',colour,'Linestyle',linestyle,'Linewidth',2)
+        line(count)=plot(time,Data.AVE.(Tone_side{tone_side}).(Hemi{hemi}),'Color',colour,'Linestyle',linestyle,'Linewidth',2);
             %make standard error
             SE_upper=[];
             SE_lower=[];
@@ -677,7 +677,7 @@ legend(line,{'ToneR-hr' 'ToneR-hl' 'ToneL-hr' 'ToneL-hl'})
 subplot(5,4,[5 6 9 10])
 hold on
 %effect
-sig_t=[]
+sig_t=[];
 sig_temp=find(Main1_abs==1);
 if ~isempty(sig_temp)
     sig_t(1)=sig_temp(1);
@@ -705,7 +705,7 @@ for tone_side=1:length(Tone_side)
     if tone_side==1
         colour=[.625 0 0];
     else
-        colour=[0 .312 .625]
+        colour=[0 .312 .625];
     end
     count=count+1;
     set(gca, 'YDir','reverse')
@@ -720,7 +720,7 @@ for tone_side=1:length(Tone_side)
     end
     data_temp1=Data.AVE.(Tone_side{tone_side}).(Hemi{1});
     data_temp2=Data.AVE.(Tone_side{tone_side}).(Hemi{2});
-    line(count)=plot(time,mean([data_temp1,data_temp2],2),'Color',colour,'Linestyle','--','Linewidth',2)
+    line(count)=plot(time,mean([data_temp1,data_temp2],2),'Color',colour,'Linestyle','--','Linewidth',2);
         %make standard error
         SE_upper=[];
         SE_lower=[];
@@ -790,7 +790,7 @@ for hemi=1:length(Hemi)
     end
     data_temp1=Data.AVE.(Tone_side{1}).(Hemi{hemi});
     data_temp2=Data.AVE.(Tone_side{2}).(Hemi{hemi});
-    line(count)=plot(time,mean([data_temp1,data_temp2],2),'Color',colour,'Linestyle',linestyle,'Linewidth',2)
+    line(count)=plot(time,mean([data_temp1,data_temp2],2),'Color',colour,'Linestyle',linestyle,'Linewidth',2);
         %make standard error
         SE_upper=[];
         SE_lower=[];
@@ -838,7 +838,7 @@ legend(line,{'Hemi r', 'Hemi l'})
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Test={'Peak Width','Peak Latency','Slope_Pre_Peak','Slope_Post_Peak'}
-test=1
+test=2
 
 Partic=1:10;
 Tone_side={'RE','LE'};
@@ -1006,7 +1006,7 @@ subplot(3,2,[5 6])
 hold on
 
 
-line=[]
+line=[];
 count=0;
 for tone_categ=1:length(Categ)
     if tone_categ==1
@@ -1223,3 +1223,407 @@ for hemi=1:length(Hemi)
 end
 legend(line,{'Hemi r', 'Hemi l'})
 
+
+
+
+
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ANOVA: Each peak amp and lat
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Test={'Amp','Lat'}
+test=2
+Peaks={'P1','N1','P2'};
+peak=2
+
+Peak_avg=0 %if true, take avg of window around peak
+
+Partic=1:10;
+Tone_side={'RE','LE'};
+Hemi={'rig','lef'};
+Categ={'contra','ipsi'};
+time=linspace(-201.000005, 793.699980,207);
+peak_windows=[35 75;80 120; 130 210];
+
+%for ANOVA   
+within1=categorical([1 1 0 0])';
+within2=categorical([1 0 1 0])';
+within=table(within1,within2,'variablenames',{'Tone','Hemi'});
+            
+%loop through time
+Main1_p=[];
+Main2_p=[];
+Interact_p=[];
+Main1_F=[];
+Main2_F=[];
+
+data=[];
+count=0;
+for tone_categ=1:length(Categ)
+    for hemi=1:length(Hemi)
+        if hemi==1%right hemi
+            if tone_categ==1%contra
+                tone_side=2;%left tone
+            else%ipsi
+                tone_side=1;%right
+            end
+        else%left hemi
+             if tone_categ==1%contra
+                tone_side=1;
+            else%ipsi
+                tone_side=2;
+             end
+        end
+        count=count+1;       
+        for partic=1:length(Partic)
+                % detect peak: P1
+                dataoi=Data.(strcat('S',num2str(partic))).(Tone_side{tone_side}).(Hemi{hemi}).*(-1);
+                timeoi=time>peak_windows(peak,1) & time<=peak_windows(peak,2);
+                if peak==2
+                    [peak_amp,peak_i]=min(dataoi(timeoi));
+                else
+                    [peak_amp,peak_i]=max(dataoi(timeoi));
+                end
+                peak_lat=time(timeoi);
+                peak_lat=peak_lat(peak_i);
+%                 %plot
+%                 figure
+%                 plot(time,dataoi,'k')
+%                 hold on
+%                 plot(time(timeoi),dataoi(timeoi),'k','Linewidth',2)
+%                 plot(peak_lat,peak_amp,'ro','Linewidth',2)
+                if test==1%amp
+                    data(partic,count)=peak_amp;
+                else
+                    data(partic,count)=peak_lat;
+                end
+        end
+    end
+end
+keep_test_data=data;
+tab=array2table(data,'variablenames',{'Contra_hr' 'Contra_hl' 'Ipsi_hr' 'Ipsi_hl'});
+rm=fitrm(tab,'Contra_hr-Ipsi_hl ~1','WithinDesign',within);
+ranovatbl = ranova(rm,'withinmodel','Tone*Hemi');
+%     a(t,:)=[time(t),table2array(ranovatbl(5,5))];
+Main1_p=table2array(ranovatbl(3,5));
+Main2_p=table2array(ranovatbl(5,5));
+Interact_p=table2array(ranovatbl(7,5));
+Main1_F=table2array(ranovatbl(3,4));
+Main2_F=table2array(ranovatbl(5,4));
+Interact_F=table2array(ranovatbl(7,4));
+
+
+
+
+
+
+
+
+
+%% PLOT
+
+yrange=[-20 60];
+figure
+clf
+Categ={'contra','ipsi'};
+%plot first 4
+
+for hemi=1:length(Hemi)
+    subplot(3,2,hemi)
+    count=0;
+    line=[];
+    for tone_categ=1:length(Categ)
+        count=count+1;
+        if tone_categ==1
+            colour='k';%[.625 0 0];
+        else
+            colour='r';%[0 .312 .625];
+        end
+
+        if hemi==1%right hemi
+            if tone_categ==1;%contra
+                tone_side=2;%left tone
+            else%ipsi
+                tone_side=1;%right
+            end
+        else%left hemi
+             if tone_categ==1;%contra
+                tone_side=1;
+            else%ipsi
+                tone_side=2;
+             end
+        end
+            
+       
+        if hemi==1
+            linestyle='-';
+        else
+            linestyle=':';
+        end
+        
+        set(gca, 'YDir','reverse');
+        if count==1
+            ylabel('Amplitude (nAm)');
+        end
+        xlabel('Time (ms)');
+        hold on
+        title(strcat('Hemi: ', Hemi{hemi}(1)));
+        data_oi=[];
+        for partic=1:length(Partic)
+            data=Data.(strcat('S',num2str(partic))).(Tone_side{tone_side}).(Hemi{hemi});
+%             plot(time,data)
+            data_oi(partic,:)=data;
+        end
+        line(count)=plot(time,Data.AVE.(Tone_side{tone_side}).(Hemi{hemi}),'Color',colour,'Linestyle',linestyle,'Linewidth',2);
+            %make standard error
+            SE_upper=[];
+            SE_lower=[];
+            for i=1:length(time)
+                se=std(data_oi(:,i))./sqrt(length(data_oi(:,i)));
+                SE_upper(i)=mean(data_oi(:,i))+se;
+                SE_lower(i)=mean(data_oi(:,i))-se;
+            end
+            tempx=[time,fliplr(time)];
+            tempy=[SE_upper,fliplr(SE_lower)];
+            A=fill(tempx,tempy,'k');
+            A.EdgeColor='none';
+            A.FaceColor=colour;
+            A.FaceAlpha=.2;
+            pause(.1)
+        ylim(yrange)
+        xlim([0 200])
+    end
+    legend(line,Categ)
+end
+
+
+%plot big one withinteraction
+subplot(3,2,[5 6])
+hold on
+
+
+line=[];
+count=0;
+for tone_categ=1:length(Categ)
+    if tone_categ==1
+        colour='k';%[.625 0 0];
+    else
+        colour='r';%[0 .312 .625];
+    end
+    for hemi=1:length(Hemi)
+
+        if hemi==1%right hemi
+            if tone_categ==1;%contra
+                tone_side=2;%left tone
+            else%ipsi
+                tone_side=1;%right
+            end
+        else%left hemi
+             if tone_categ==1;%contra
+                tone_side=1;
+            else%ipsi
+                tone_side=2;
+             end
+        end
+         
+        if hemi==1
+            linestyle='-';
+        else
+            linestyle=':';
+        end
+        count=count+1;
+        set(gca, 'YDir','reverse')
+        ylabel('Amplitude (nAm)')
+        xlabel('Time (ms)')
+        hold on
+        if Interact_p>.05
+            title('Interaction')
+        else
+            title(strcat('Tone Effect* - p = ',num2str(Interact_p)))
+        end        
+        data_oi=[];
+        for partic=1:length(Partic)
+            data=Data.(strcat('S',num2str(partic))).(Tone_side{tone_side}).(Hemi{hemi});
+%             plot(time,data)
+            data_oi(partic,:)=data;
+        end
+        line(count)=plot(time,Data.AVE.(Tone_side{tone_side}).(Hemi{hemi}),'Color',colour,'Linestyle',linestyle,'Linewidth',2);
+        dataoi=Data.AVE.(Tone_side{tone_side}).(Hemi{hemi});
+        if test==2
+            plot([mean(keep_test_data(:,count)),mean(keep_test_data(:,count))], yrange,'Color',colour,'Linestyle',linestyle,'Linewidth',2);
+        elseif test==3 | test==4
+            P=polyfit(time(slope_window_ind(1):slope_window_ind(2))',dataoi(slope_window_ind(1):slope_window_ind(2)),1);
+            plot(time(slope_window_ind(1):slope_window_ind(2)), polyval(P,time(slope_window_ind(1):slope_window_ind(2))),'Color',colour,'Linewidth',5);
+        end
+            %make standard error
+            SE_upper=[];
+            SE_lower=[];
+            for i=1:length(time)
+                se=std(data_oi(:,i))./sqrt(length(data_oi(:,i)));
+                SE_upper(i)=mean(data_oi(:,i))+se;
+                SE_lower(i)=mean(data_oi(:,i))-se;
+            end
+            tempx=[time,fliplr(time)];
+            tempy=[SE_upper,fliplr(SE_lower)];
+            A=fill(tempx,tempy,'k');
+            A.EdgeColor='none';
+            A.FaceColor=colour;
+            A.FaceAlpha=.2;
+            pause(.1)
+        ylim(yrange)
+        xlim([0 200])
+    end
+end
+set(gca, 'YDir','reverse')
+legend(line,{'Contra-hr' 'Contra-hl' 'Ipsi-hr' 'Ipsi-hl'})
+
+
+% plot main effects
+subplot(3,2,3)
+hold on
+if Main1_p>.05
+    title('Tone Effect');
+else
+    title(strcat('Tone Effect* - p = ',num2str(Main1_p)))
+end
+line=[];
+count=0;
+for tone_categ=1:length(Categ)
+    if tone_categ==1
+        colour='k';%[.625 0 0];
+    else
+        colour='r';%[0 .312 .625];
+    end
+
+    count=count+1;
+    set(gca, 'YDir','reverse')
+    ylabel('Amplitude (nAm)')
+    hold on
+    data_oi=[];
+    for partic=1:length(Partic)
+        if tone_categ==1
+            data1=Data.(strcat('S',num2str(partic))).(Tone_side{1}).(Hemi{2});
+            data2=Data.(strcat('S',num2str(partic))).(Tone_side{2}).(Hemi{1});
+        else
+            data1=Data.(strcat('S',num2str(partic))).(Tone_side{1}).(Hemi{1});
+            data2=Data.(strcat('S',num2str(partic))).(Tone_side{2}).(Hemi{2});
+        end
+%             plot(time,data)
+        data_oi(partic,:)=mean([data1,data2],2);
+    end
+    if tone_categ==1
+        data_temp1=Data.AVE.(Tone_side{1}).(Hemi{2});
+        data_temp2=Data.AVE.(Tone_side{2}).(Hemi{1});
+    else
+        data_temp1=Data.AVE.(Tone_side{1}).(Hemi{1});
+        data_temp2=Data.AVE.(Tone_side{2}).(Hemi{2});
+    end
+    dataoi=mean([data_temp1,data_temp2],2);
+    line(count)=plot(time,dataoi,'Color',colour,'Linestyle','--','Linewidth',2);
+    if test==2
+        if tone_categ==1
+            temp=[1 2];
+        else
+            temp=[3,4];
+        end
+            plot([mean([keep_test_data(:,temp(1));keep_test_data(:,temp(2))] ),mean([keep_test_data(:,temp(1));keep_test_data(:,temp(2))] )], yrange,'Color',colour,'Linestyle',linestyle,'Linewidth',2);
+    elseif test==3 | test==4
+        P=polyfit(time(slope_window_ind(1):slope_window_ind(2))',dataoi(slope_window_ind(1):slope_window_ind(2)),1);
+        plot(time(slope_window_ind(1):slope_window_ind(2)), polyval(P,time(slope_window_ind(1):slope_window_ind(2))),'Color',colour,'Linewidth',5);
+    end
+    
+    %make standard error
+        SE_upper=[];
+        SE_lower=[];
+        for i=1:length(time)
+            se=std(data_oi(:,i))./sqrt(length(data_oi(:,i)));
+            SE_upper(i)=mean(data_oi(:,i))+se;
+            SE_lower(i)=mean(data_oi(:,i))-se;
+        end
+        tempx=[time,fliplr(time)];
+        tempy=[SE_upper,fliplr(SE_lower)];
+        A=fill(tempx,tempy,'k');
+        A.EdgeColor='none';
+        A.FaceColor=colour;
+        A.FaceAlpha=.2;
+        pause(.1)
+    ylim(yrange)
+    xlim([0 200])
+end
+% plot(time,Main1_abs*55,'ko')
+legend(line, Categ)
+
+
+subplot(3,2,4)
+hold on
+%effect
+
+if Main2_p>.05
+    title('Hemi Effect');
+else
+    title(strcat('Hemi Effect* - p = ',num2str(Main2_p)))
+end
+line=[];
+count=0;
+for hemi=1:length(Hemi)
+    colour=[.2 .2 .2];
+    if hemi==1
+        linestyle='-';
+    else
+        linestyle=':';
+    end
+    count=count+1;
+    set(gca, 'YDir','reverse')
+    ylabel('Amplitude (nAm)')
+    hold on
+
+    data_oi=[];
+    for partic=1:length(Partic)
+        data1=Data.(strcat('S',num2str(partic))).(Tone_side{1}).(Hemi{hemi});
+        data2=Data.(strcat('S',num2str(partic))).(Tone_side{2}).(Hemi{hemi});
+%             plot(time,data)
+        data_oi(partic,:)=mean([data1,data2],2);
+    end
+    data_temp1=Data.AVE.(Tone_side{1}).(Hemi{hemi});
+    data_temp2=Data.AVE.(Tone_side{2}).(Hemi{hemi});
+    dataoi=mean([data_temp1,data_temp2],2);
+    line(count)=plot(time,dataoi,'Color',colour,'Linestyle',linestyle,'Linewidth',2);
+    if test==2
+        if hemi==1
+            temp=[1 3];
+        else
+            temp=[2,4];
+        end
+            plot([mean([keep_test_data(:,temp(1));keep_test_data(:,temp(2))] ),mean([keep_test_data(:,temp(1));keep_test_data(:,temp(2))] )], yrange,'Color',colour,'Linestyle',linestyle,'Linewidth',2);
+    elseif test==3 | test==4
+        P=polyfit(time(slope_window_ind(1):slope_window_ind(2))',dataoi(slope_window_ind(1):slope_window_ind(2)),1);
+        plot(time(slope_window_ind(1):slope_window_ind(2)), polyval(P,time(slope_window_ind(1):slope_window_ind(2))),'Color',colour,'Linewidth',5);
+    end
+        %make standard error
+        SE_upper=[];
+        SE_lower=[];
+        for i=1:length(time)
+            se=std(data_oi(:,i))./sqrt(length(data_oi(:,i)));
+            SE_upper(i)=mean(data_oi(:,i))+se;
+            SE_lower(i)=mean(data_oi(:,i))-se;
+        end
+        tempx=[time,fliplr(time)];
+        tempy=[SE_upper,fliplr(SE_lower)];
+        A=fill(tempx,tempy,'k');
+        A.EdgeColor='none';
+        A.FaceColor=colour;
+        A.FaceAlpha=.2;
+        pause(.1)
+    ylim(yrange)
+    xlim([0 200])
+end
+legend(line,{'Hemi r', 'Hemi l'})
