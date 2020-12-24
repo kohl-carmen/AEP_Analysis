8021e1c6224f023dea0ab5740bd468c3d14524c6
Carmen <kohl.carmen.1@gmail.com>
2020-12-24 17:37:49 -0500

    updated Figure5 (all models)

--- AEP_Paper/Plot_Contra_to_Ipsi.m
+++ AEP_Paper/Plot_Contra_to_Ipsi.m
@@ -1,21 +1,22 @@
 %% Plot Contra to Ipsi Step Figure
 % Based on ../Contra_to_Ipsi_Plot.m
-% this creates Figure 4
+% this creates Figure 5
 % - loads Tiina's MEG data and plots the AEP for right/left contra/ipsi
-% - loads models for contra, ipsi, and step
+% - loads models for contra/ipsi, left/right
 %       - contra:   og fitted model
-%       - step:     1st step towards ipsi: change only scaling param
 %       - ipsi:     2nd step: change input latencies
 % - puts eps file on Desktop
 
 clear
-set(gcf,'renderer','painters')
+file_dir='C:\Users\ckohl\Desktop\Current\AEP\Data\Tiina New Data\alternate_auditory_toCarmen_edited_for_import.xls';
+modeldata_dir = 'C:\Users\ckohl\hnn_out\data\';
+output_dir = 'C:\Users\ckohl\Desktop\';
 
+set(gcf,'renderer','painters')
 Partic=1:10;
 Tone_side={'RE','LE'};
 Hemi={'rig','lef'};
 Categ={'contra','ipsi'};
-modeldata_dir='C:\Users\ckohl\hnn_out\data\';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Load data
@@ -30,7 +31,7 @@ opts.VariableNames = ["S1_LE_lef", "S2_LE_lef", "S3_LE_lef", "S4_LE_lef", "S5_LE
 opts.SelectedVariableNames = ["S1_LE_lef", "S2_LE_lef", "S3_LE_lef", "S4_LE_lef", "S5_LE_lef", "S6_LE_lef", "S7_LE_lef", "S8_LE_lef", "S9_LE_lef", "S10_LE_lef", "AVE_LE_lef", "S1_LE_rig", "S2_LE_rig", "S3_LE_rig", "S4_LE_rig", "S5_LE_rig", "S6_LE_rig", "S7_LE_rig", "S8_LE_rig", "S9_LE_rig", "S10_LE_rig", "AVE_LE_rig", "S1_RE_lef", "S2_RE_lef", "S3_RE_lef", "S4_RE_lef", "S5_RE_lef", "S6_RE_lef", "S7_RE_lef", "S8_RE_lef", "S9_RE_lef", "S10_RE_lef", "AVE_RE_lef", "S1_RE_rig", "S2_RE_rig", "S3_RE_rig", "S4_RE_rig", "S5_RE_rig", "S6_RE_rig", "S7_RE_rig", "S8_RE_rig", "S9_RE_rig", "S10_RE_rig", "AVE_RE_rig"];
 opts.VariableTypes = ["double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double", "double"];
 % Import the data
-tbl = readtable("C:\Users\ckohl\Desktop\Current\AEP\Data\Tiina New Data\alternate_auditory_toCarmen_edited_for_import.xls", opts, "UseExcel", false);
+tbl = readtable(file_dir, opts, "UseExcel", false);
 
 %% Convert to output type
 Data.S1.LE.lef = tbl.S1_LE_lef;
@@ -83,12 +84,11 @@ data_time=linspace(-201.000005, 793.699980,207);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Contra-to-Ipsi Step Figure
+%% Contra-to-Ipsi Figure
 ylims=[-60 20];
 xlims=[0 250];
 figure
 clf
-Steps={'Contra','Step', 'Ipsi'};
 Hemi={'R' 'L'};
 idx=0;
 for hemi=1:length(Hemi)
@@ -96,37 +96,43 @@ for hemi=1:length(Hemi)
     if hemi==1 %R
         data_contra=Data.AVE.LE.rig;
         data_ipsi=Data.AVE.RE.rig;
+        hemi_i=2;
     else %L
         data_contra=Data.AVE.RE.lef;
         data_ipsi=Data.AVE.LE.lef;
         idx=3;
+        hemi_i=1;
     end   
-    for step=1:length(Steps)
-        subplot(length(Hemi),length(Steps),step+idx)
-        hold on   
-        title(strcat(Hemi{hemi},'-',Steps{step}))
-        %plot data
-        plot(data_time, data_contra.*-1,'Color',[226 113 113]./255,'Linewidth',3)
-        plot(data_time, data_ipsi.*-1,'Color',[109 187 228]./255,'Linewidth',3)
-        
-        %load model
-        param_name=strcat(Hemi{hemi},'_',Steps{step});
-        dpl=load(strcat(modeldata_dir,'\',param_name,'\dpl.txt'));
-        sim_time=dpl(:,1);
-        dpl_agg=dpl(:,2);%aggregate
-        %plot model
-        plot(sim_time,dpl_agg,'k','Linewidth',2)
-        xlim(xlims)
-        ylim(ylims)
-    end
+
+    subplot(1,length(Hemi),hemi_i)
+    hold on   
+    title(strcat(Hemi{hemi}))
+    %plot data
+    plot(data_time, data_contra.*-1,'Color',[109 187 228]./255,'Linewidth',4)
+    plot(data_time, data_ipsi.*-1,'Color',[226 113 113]./255,'Linewidth',4)
+    
+    %load and plot contra
+    param_name=strcat(Hemi{hemi},'_Contra');
+    dpl=load(strcat(modeldata_dir,'\',param_name,'\dpl.txt'));
+    sim_time=dpl(:,1);
+    dpl_agg=dpl(:,2);%aggregate
+    plot(sim_time,dpl_agg,'Color',[41 47 123]./255,'Linewidth',2)
+    %load and plot ipsi
+    param_name=strcat(Hemi{hemi},'_Ipsi');
+    dpl=load(strcat(modeldata_dir,'\',param_name,'\dpl.txt'));
+    sim_time=dpl(:,1);
+    dpl_agg=dpl(:,2);%aggregate
+    plot(sim_time,dpl_agg,'Color',[126 20 22]./255,'Linewidth',2)
+    
+    xlim(xlims)
+    ylim(ylims)
+
 end
-legend('contra data','ipsi data','model')        
-
-
+legend('contra data','ipsi data','contra model', 'ipsi model')        
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Save
-cd('C:\Users\ckohl\Desktop\')
-print 'Figure_4_Raw' -depsc -painters
+cd(output_dir)
+print 'Figure_5_Raw' -depsc -painters
 
 
